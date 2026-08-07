9760370bdc5682f53b7ac1b3a9b960f514dd0a5d
Fatima <muqadasf852@gmail.com>
2026-08-07 17:03:07 +0500

    Added second chapter named Variables and Data Types to python.

---
+++ Chapter-2/1- Variables.py
@@ -0,0 +1,10 @@
+a = 1   # variables = container to store a value.
+
+b = 2
+
+print(a+b)
+c = "Fatima"   #keywords = reserved words in python which have special meaning and cannot be used as variable names.
+
+d = 7.221     #identifiers = names given to variables, functions, classes, etc. in Python.
+
+print(b+d)
