ff4d80894c6daeeabe6f98e9623724729af6d404
Fatima <muqadasf852@gmail.com>
2026-08-07 17:48:29 +0500

    Added another file in chapter 2 f python named Data Type.

---
+++ Chapter-2/2- Data Type.py
@@ -0,0 +1,5 @@
+a = 4        # identifies a class <int>
+b = 79.15    # identifies b class <float>
+c = "Fatima"   # identifies c class <str>
+d = True     # identifies d class <bool>
+e = None     # identifies e class <NoneType>
