b95dec8a8c3fc790c86ef5dc29436260bb315d2c
Fatima <muqadasf852@gmail.com>
2026-08-08 13:46:49 +0500

    Learned python operators.

---
+++ Chapter-2/3- Operators.py
@@ -0,0 +1,28 @@
+               # Arithmetic Operators
+print(5 + 3) 
+
+print(9-4)
+
+print(5*7)
+
+print(100/10)
+a = 10
+b = 7.42
+print(a*b)
+
+         # Assignment Operators
+x = 9-4
+print(x)
+
+y = 5
+y += 8           # increment 'y' by 8 and assign new value to 'y'
+print(y)
+
+z = 6
+z -= 3           # decrement 'z' by 3 and assign new value to 'z'
+print(z)
+
+                  # Comparison Operators
+print(5 == 5)    # Equal to
+d = 5<2
+print(d)
