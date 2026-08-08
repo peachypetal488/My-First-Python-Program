977c848c866dfb5ae82923ccc0696b00a149b2c2
Fatima <muqadasf852@gmail.com>
2026-08-08 15:31:19 +0500

    Added comparison and logical operators.

--- Chapter-2/3- Operators.py
+++ Chapter-2/3- Operators.py
@@ -24,5 +24,29 @@ print(z)
 
                   # Comparison Operators
 print(5 == 5)    # Equal to
-d = 5<2
+d = 5<2          # Less than
 print(d)
+e = 5==5
+print(e)
+print(5!=5)    # Not equal to
+print(5>2)     # Greater than
+print(5>=5)    # Greater than or equal to
+f = 5<=2     # Less than or equal to
+print(f)
+
+              # Logical Operators
+# Truth table for AND operator
+print("True and True is", True and True)
+print("True and False is", True and False)
+print("False and True is", False and True)
+print("False and False is", False and False)
+
+# Truth table for OR operator
+print("True or True is", True or True)
+print("True or False is", True or False)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table for NOT operator
+print("not True is", not True)
+print("not False is", not False)
